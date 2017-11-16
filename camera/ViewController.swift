c431a44e5559f0108caa2ff96f3adc3f0d403c74
Cyberjaya 4 iTrain <cyberjaya4itrain@Cyberjayas-iMac-6.local>
2017-11-16 14:34:47 +0800

    first commit

--- camera/ViewController.swift
+++ camera/ViewController.swift
@@ -8,13 +8,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    @IBOutlet var myImageView: UIImageView!
+    
+    @IBAction func action_camera(_ sender: Any) {
+    
+    
+    
+    }
+   
+    @IBAction func action_photo(_ sender: Any) {
+     let myPicker = UIImagePickerController()
+        myPicker.delegate = self
+        myPicker.sourceType = .camera
+        
+        present(myPicker, animated: true, completion: nil)
+        
+    }
+    
 
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        let selectedImage = info[UIImagePickerControllerOriginalImage] as? UIImage
+        
+        self.myImageView.image = selectedImage
+        
+        
+        //close the panel
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
